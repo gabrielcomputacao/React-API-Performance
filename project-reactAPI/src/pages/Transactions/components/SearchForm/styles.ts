49d41319423015475d1d261039405c430a6dcc00
gabriel <gabrielcomputacao@outlook.com>
2023-10-16 06:54:59 -0300

    feat: creating components transactions and form

---
+++ project-reactAPI/src/pages/Transactions/components/SearchForm/styles.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+    border-radius: 6px;
+    border: 0;
+    background: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
+    padding: 1rem;
+
+    &::placeholder {
+      color: ${(props) => props.theme["gray-500"]};
+    }
+  }
+  button {
+    display: flex;
+    gap: 0.75rem;
+    align-items: center;
+
+    border: 0;
+    background: transparent;
+    padding: 1rem;
+    border: 1px solid ${(props) => props.theme["green-300"]};
+    font-weight: bold;
+    border-radius: 6px;
+    color: ${(props) => props.theme["green-300"]};
+
+    &:hover {
+      background: ${(props) => props.theme["green-500"]};
+
+      border-color: 1px solid ${(props) => props.theme["green-500"]};
+
+      color: ${(props) => props.theme.white};
+      transition: background-color 0.2s, color 0.2s;
+    }
+  }
+`;
--- project-reactAPI/src/pages/Transactions/index.tsx
+++ project-reactAPI/src/pages/Transactions/index.tsx
@@ -1,11 +1,41 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%"> Desenvolvimento Site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 1000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%"> Comida</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
